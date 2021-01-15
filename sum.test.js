6ce32d5b56d2e3c3fd8727614b00abaa8baf1a3b
nishant <nishant.singh87@gmail.com>
2021-01-15 18:06:59 +0530

    polish chai messages

--- sum.test.js
+++ sum.test.js
@@ -1,14 +1,14 @@
 const expect = require('chai').expect;
 
 const specs = [
-  {feature : "test-1", scenario: "test-1/one", value: true},
-  {feature : "test-1", scenario: "test-1/two", value: true},
-  {feature : "test-2", scenario: "test-2/one", value: false},
-  {feature : "test-3", scenario: "test-3/one", value: true},
+  {feature : "test-1", scenario: "test-1/one", expected: {tree: {branchOne: {name : "one", values: [3,4, 1]}}} ,actual: {tree: {branchOne: {name : "one", values: [3,3, 1]}}}},
+  {feature : "test-1", scenario: "test-1/two", expected: false ,actual: false},
+  {feature : "test-2", scenario: "test-2/one", expected: false,actual: false},
+  {feature : "test-3", scenario: "test-3/one", expected: false ,actual: false},
 ]
 
 const runSpec = async (spec) => {
-  expect(spec.value).to.equal(true);
+  expect(spec.actual).to.equal(spec.expected);
 };
 
 const runTests = async () => {
