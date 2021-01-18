813637f1f74e2d3359cbfa7600d1a0a3de61a3c9
nishant <nishant.singh87@gmail.com>
2021-01-18 16:30:50 +0530

    Get url in variables script.

--- example/e2e/scripts/tokens.variable.js
+++ example/e2e/scripts/tokens.variable.js
@@ -1,10 +1,8 @@
 const axios = require("axios");
 
 module.exports = async (ispec) => {
-  const response = await axios({
-    url: 'http://localhost:3123/get-token?user=guest&access=invitation',
-    method: 'GET'
-  });
+  const url = ispec.getUrl('/get-token?user=guest&access=invitation');
+  const response = await axios({url});
 
   const token = response.data.token;
   ispec.addVariable({guestToken: token});
