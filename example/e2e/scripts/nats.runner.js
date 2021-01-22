bac477e02d5fdff37076b5527d91cd653a01abe4
nishant <nishant.singh87@gmail.com>
2021-01-22 14:23:12 +0530

    Add e2e tests with nats runner.

--- example/e2e/scripts/nats.runner.js
+++ example/e2e/scripts/nats.runner.js
@@ -1,16 +1,32 @@
+const NATS = require("nats");
+
 module.exports = async (ispec) => {
-  console.log("nats runner initiated.");
+  const natsUrl = process.env.nats_endpoint || 'localhost:4222';
+  const nats = NATS.connect(natsUrl, {json:true});
+  console.log("Runner connected to NATS at : ", natsUrl);
 
   const runner = {
     appliesTo: (specAsJson) => {
       return specAsJson.hasOwnProperty('nats');
     },
 
-    beforeSpec: (specAsJson, ispec) => {
-      console.log("setting up nats");
+    beforeSpec: async (specAsJson, ispec) => {
+      for(const {topic, reply, expectedMessage} of specAsJson.nats.given){
+        console.log("setting up nats subscriptions..")
+        nats.subscribe(topic, (message, replyTo) => {
+          nats.publish(replyTo, reply);
+        });
+      }
+
+      // ensure nats subscription is ready before proceeding
+      await new Promise((resolve) => {
+        nats.subscribe('__nats-runner.wait-till-ready', resolve);
+        nats.publish('__nats-runner.wait-till-ready');
+      });
     },
 
     afterSpec: (specAsJson, expect) => {
+      nats.close();
       return expect([
           {
             expected: {value: [1, 2 ,3]},
