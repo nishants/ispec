1d852eefabbee9f5896a28ff0b7483fdd3bc3548
nishant <nishant.singh87@gmail.com>
2021-01-22 14:51:23 +0530

    Ensure data is published to nats in example service.

--- example/src/endpoints/nats/nats-client.js
+++ example/src/endpoints/nats/nats-client.js
@@ -2,10 +2,12 @@ const NATS = require("nats");
 const natsUrl = process.env.nats_endpoint || 'localhost:4222';
 const nats = NATS.connect(natsUrl, {json:true});
 
-const sendMessage = (sessionId, message) => {
-  nats.request(`search.find-session.${sessionId}`, {sessionId, message},(serverName) => {
-    nats.publish(`server.${serverName}`, message);
-  });
+const sendMessage = async (sessionId, message) => {
+  return new Promise((resolve, reject) => {
+    nats.request(`search.find-session.${sessionId}`, {sessionId, message},(serverName) => {
+      nats.publish(`server.${serverName}`, message, resolve);
+    });
+  })
 };
 
 const pingServer = (serverName, timeout = 100) => {
