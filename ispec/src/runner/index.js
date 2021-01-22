14062b6235e57c3a480fac08c2948e0b66e56373
nishant <nishant.singh87@gmail.com>
2021-01-22 15:45:29 +0530

    Match assertions from runner.

--- ispec/src/runner/index.js
+++ ispec/src/runner/index.js
@@ -3,6 +3,7 @@ const mustache = require('mustache');
 const {readYamlFile} = require('../utils/file')
 const runners = [];
 const defaultRunner = require('./default-runner');
+const util = require('util');
 
 module.exports = {
   add : (name, runner) => {
@@ -13,9 +14,34 @@ module.exports = {
     const specAsJson = JSON.parse(mustache.render(JSON.stringify(specTemplate), ispec.variables()));
     const appliedRunners = runners.filter(r => r.appliesTo(specAsJson));
 
+    const runnerAssertionFailures = [];
+
     const runnerExpect = (assertion) => {
-      console.log(assertion)
+      const status = util.isDeepStrictEqual(assertion.expected, assertion.actual);
+      if(!status){
+        runnerAssertionFailures.push({
+          message: assertion.message,
+          expected: assertion.expected,
+          actual: assertion.actual,
+        });
+      }
     };
+
+    const combineRunnerStatus = (defaultStatus) => {
+      const result = {
+        success: defaultStatus.success && runnerAssertionFailures.length === 0,
+        expected : {...defaultStatus.expected},
+        actual : {...defaultStatus.actual}
+      };
+
+      for(const failure of runnerAssertionFailures){
+        result.expected[failure.message] = failure.expected;
+        result.actual[failure.message] = failure.actual;
+      }
+
+      return result;
+    };
+
     const before = async () => {
 
       for(const runner of appliedRunners){
@@ -34,7 +60,7 @@ module.exports = {
     await after();
 
     return {
-      status: status,
+      status: combineRunnerStatus(status),
       spec: specAsJson.spec
     };
   }
